b109a4359647f6adad1626902a28916a345ebff7
frutos93 <cefg93@gmail.com>
2016-10-07 07:52:44 -0500

    Problems with the different views.

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.common')
+@extends('layouts.layout')
 
 @section('content')
         <!-- Page Content -->
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('gallery');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/gallery', 'HomeController@gallery');
