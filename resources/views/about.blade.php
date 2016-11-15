d4e38fb18ad634a8a4f5bce766a5494d122a2ef0
AndresAvila <andres.amdc@gmail.com>
2016-11-14 20:59:34 -0600

    Información

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,140 @@
-@extends('layouts.common')
+@extends('layouts.layout')
+
+@section('content')
+
+<link href="css/app.css" rel="stylesheet">
+
+<section id="estadisticas">
+    <div class="container">
+        <div class="page-header text-center">
+            <h1>
+                Historia
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    Somos una Asociación Civil conformada por estudiantes de distintas universidades de México que por medio del voluntariado promovemos el desarrollo social, el amor a la educación, el cuidado del medio ambiente, la importancia de los valores y el deseo de progresar como ciudadanos en los niños de casas hogar y escasos recursos. Iniciamos como un grupo estudiantil en el 2007, y después de varios años hemos logrado consolidarnos como una Asociación Civil.
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h2>
+                Misión
+            </h2>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    Ser un grupo de jóvenes voluntarios comprometidos en transformar comunidades en situación de pobreza al destacar e impulsar el papel de los NIÑOS COMO AGENTES DE CAMBIO a través de un modelo educativo integral que tenga como base el AMOR A LA EDUCACIÓN y el deseo de desarrollo.
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Visión
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    Replicar un modelo de desarrollo de competencias, consolidado por el aprendizaje conjunto e incluyente de las diferentes comunidades e instituciones del país.
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Virtudes
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    En Ohana nos interesa desarrollar las virtudes en los niños y voluntarios porque queremos que más que identificar consecuencias que desean evitar que identifiquen la honestidad como virtud y se guíen por ella Es difícil hacer que tus emociones se alineen con tus razón, ser virtuoso lleva tiempo y esfuerzo.
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Alegría
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    Normalmente considerada una emoción, si la alegría es internalizada, resulta en una persona fresca y luminosa, dándole así una poderosa disposición, que resulta en una mentalidad constructiva. Es la virtud que se debe inculcar y aprender primero, una vez que se es una persona alegre, es más fácil escuchar y estar inclinado a buscar las demás virtudes.
+
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Valentía
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    También conocida como coraje, es fuerza de voluntad que te permite realizar una acción a pesar de los impedimentos. Pensando en Aristóteles y su punto medio, se debe entender que coraje no es enfrentar todo lo que se te atraviese sin reflexionarlo. Eso sería un exceso que se conoce como ser temerario. El punto medio entre ser temerario y cobarde es el coraje, lo importante es saber buscar dónde se encuentra ese balance.
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Fortaleza
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    Una persona que posee la virtud de la fortaleza, es firme ante las dificultades y está en constante búsqueda del bien. Ordena las emociones en cuanto a la razón y no sigue sus impulsos. Cabe destacar que se requiere el coraje para enfrentarse a las dificultades, es decir, sin coraje no hay fortaleza. 
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Perseverancia
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    La perseverancia nos enseña a concentrarnos y a seguir con tenacidad al realizar algo sin importar las dificultades o el tiempo que nos tome lograrlo. Se requiere de fortaleza para saber qué dirección tomar. Ya que una persona desorientada y perseverante, puede lograr cosas terribles. 
+                </h3>
+            </div>
+        </div>
+        <div class="page-header text-center">
+            <h1>
+                Sabiduría
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center"> 
+                
+                <br>
+                <h3>
+                    Un sabio es, por definición, una persona que vive bien. Tiene buen juicio y ayuda a los demás a desarrollar sus virtudes. Es la última virtud, porque necesita de las virtudes anteriores y otras más para desarrollarse. Lo importante del sabio no son sus conocimientos sobre materias o temas específicos, sino su conocimiento sobre cómo vivir bien, para él y para los demás.
+                </h3>
+            </div>
+        </div>
+        
+    </div>
+</section>
+
+    @endsection
