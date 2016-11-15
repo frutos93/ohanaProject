844b6968970bea3bd531f866fb28420844e8f716
AntonioVT <tony-vt@hotmail.com>
2016-11-15 07:51:23 -0600

    Valida inicio de sesion en coordinador/participante/voluntario

--- app/Http/Controllers/CoordinadorController.php
+++ app/Http/Controllers/CoordinadorController.php
@@ -8,6 +8,13 @@ use App\Http\Requests;
 Use DB;
 class CoordinadorController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/VoluntarioController.php
+++ app/Http/Controllers/VoluntarioController.php
@@ -8,6 +8,12 @@ use App\Http\Requests;
 Use DB;
 class VoluntarioController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
