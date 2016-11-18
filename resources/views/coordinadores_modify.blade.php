ab3b9b3439aaa3a1904b5b56881c0bb6ff60314d
AntonioVT <tony-vt@hotmail.com>
2016-11-18 00:52:44 -0600

    Modificacion a forms de coordinador y web.php
    -Fix rapido de rutas, esto se tiene que corregir
    - Coordinador ya no contiene columna de email, se imprimia en el campo
    correspondiente, pero ya no.

--- resources/views/coordinadores_modify.blade.php
+++ resources/views/coordinadores_modify.blade.php
@@ -95,17 +95,6 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="name" class="col-md-4 control-label">Email</label>
-                                <div class="col-md-6">
-                                    {!! Form::text('email', $coordinador->email, [
-                                        'class' => 'form-control',
-                                        'autofocus' => 'autofocus',
-                                        'required' => 'required'
-                                    ]) !!}
-                                </div>
-                            </div>
-
-                            <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     {!! Form::submit('Modificar', [
                                         'class' => 'btn btn-primary'
